27d3c48735818579e578493313ad3c0a2e3c9ab3
HalaJah <114840619+HalaJah@users.noreply.github.com>
2023-08-24 11:40:16 -0500

    #

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -11,7 +11,7 @@ void main() => runApp(MaterialApp(
       child: const Text('Hello'),
     ),
     floatingActionButton: FloatingActionButton(
-      child: Text('click'),
+      child: icon
     ),
    )
 
