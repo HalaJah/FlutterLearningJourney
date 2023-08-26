704b40071ea65627cf3805df8c7c78634e46f94b
HalaJah <114840619+HalaJah@users.noreply.github.com>
2023-08-26 09:21:49 -0500

    Adding Columns and Rows

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-   home: Scaffold(
+   home: Home(),
+   )
+
+);
+
+class Home extends StatelessWidget
+{
+  @override
+
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
     backgroundColor: Colors.pink[100],
     appBar: AppBar(
       title: Text(
-        'First Application',
+        'Hala First Application',
         style: TextStyle(
         fontFamily: 'DancingScript',
         fontSize: 30.0,
@@ -16,10 +27,67 @@ void main() => runApp(MaterialApp(
       backgroundColor: Colors.pink[300],
     ),
     body: Center(
-      child: Image.asset('assets/images/duck.png')
-        ),
-    ),
-    
-   )
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: <Widget>[
+      Row(
+        children: <Widget>[
+          Container(
+            color: Colors.pinkAccent[200],
+            padding: EdgeInsets.all(20.0),
+            margin: EdgeInsets.all(5.0),
+            child: Row(children: <Widget> [
 
+              Column(children: <Widget> [
+              Text(
+                  'Period day 13',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    fontFamily: 'DancingScript',
+                  ),
+                ),
+                Text(
+                  'Period ends in 3 days',
+                  style: TextStyle(
+                  color: Colors.pink[600],
+                  fontSize: 10.0,
+                  fontWeight: FontWeight.bold,
+                  ),)
+              ],),
+              
+            
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                  child: Image.asset(
+                   'assets/images/duck.png',
+                    width: 100.0, 
+                    height: 100.0, 
+                  ),
+                ),
+
+                
+                    
+
+                    
+                    
+                  
+              
+                
+          
+            ],)
+                
+            ),
+        
+        ],
+      ),
+    ],
+  ),
+),
 );
+
+
+  
+  }
+
+}
