968f301c98711657c5a806a6e496daf9e0f64359
HalaJah <114840619+HalaJah@users.noreply.github.com>
2023-09-08 01:03:40 -0500

    Added title and icon to the cards stack

--- period_tracker_app/lib/customized_widgets/homeCards.dart
+++ period_tracker_app/lib/customized_widgets/homeCards.dart
@@ -13,54 +13,138 @@ class _HomeCardsState extends State<HomeCards> {
   
   Color color1 = Colors.white;
   Color color2 = Colors.white;
+  Color color3 = Colors.white;
+  IconData icon = Icons.arrow_back;
+  double width = 0.0;
+
 
   void getColor(String title){
-  if (title == 'Calendar' || title == 'Setting') {
-      color1 = Color.fromARGB(255, 238, 203, 121);
-      color2 = Color.fromARGB(255, 209, 169, 74);
+  if (title == 'Calendar' || title == 'Settings') {
+      color1 = const Color.fromARGB(255, 232, 191, 126);
+      color2 = const Color.fromARGB(255, 245, 178, 90);
+      color3 = const Color.fromARGB(255, 92, 65, 46);
     } else if (title == 'Journal') {
-      color1 = Color.fromARGB(255, 255, 192, 203);
-      color2 = Color.fromARGB(255, 252, 137, 172);
+      color1 = const Color.fromARGB(255, 255, 192, 203);
+      color2 = const Color.fromARGB(255, 252, 137, 172);
+      color3 = const Color.fromARGB(255, 179, 25, 107);
     } else if (title == 'Profile') {
-      color1 = Color.fromARGB(255, 154, 195, 236);
-      color2 = Color.fromARGB(255, 114, 157, 199);
+      color1 = const Color.fromARGB(255, 154, 195, 236);
+      color2 = const Color.fromARGB(255, 114, 157, 199);
+      color3 = const Color.fromARGB(255, 28, 70, 104);
     }
+  }
+
+  double getWidth(String title){
+    if (title == 'Calendar') {
+      return  315;
+    } else if (title == 'Journal') {
+      return 335;
+    } else if (title == 'Profile') {
+      return 355;
+    }
+    else if(title == 'Settings'){
+      return 375;
+    }
+    return 0;
+  }
+
+  IconData getIcon(String title)
+  {
+    if (title == 'Calendar') {
+      return Icons. calendar_month_rounded;
+    } else if (title == 'Journal') {
+      return  Icons.note_alt_rounded;
+    } else if (title == 'Profile') {
+      icon = Icons.edit_document;
+    }
+    else if(title == 'Settings'){
+      return  Icons.settings_rounded;
+    }
+     return icon;
   }
 
   @override
   void initState() {
   super.initState();
   getColor(widget.title);
+  width = getWidth(widget.title);
+  icon = getIcon(widget.title);
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(10.0),
+      padding: const EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
       child: Material(
-        elevation: 70,
-        child: Container(
-          alignment: Alignment.center,
-          padding: EdgeInsets.fromLTRB(40, 0, 420, 0),
-          height: 100,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(50),
-              topRight: Radius.circular(50),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey,
-                offset: Offset(0, -10),
-                blurRadius: 200,
+        color: Colors.transparent,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(35),
+          topRight: Radius.circular(35),
+          ),
+        elevation: 60,
+        child: AnimatedContainer(
+          duration: const Duration(seconds: 1),
+          curve:Curves.bounceOut, 
+          width: width,
+          child: InkWell(
+            onDoubleTap: () {
+              setState(() {
+                width == getWidth(widget.title) + 14 ? width = getWidth(widget.title) : width = width + 7;
+              });
+            },
+            splashColor: Colors.grey,
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+              alignment: Alignment.center,
+              height: 85,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(35),
+                  topRight: Radius.circular(35),
+                ),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black,
+                    offset: Offset(0, -5),
+                    blurRadius: 30,
+                    spreadRadius: 0,
+                  ),
+                ],
+                gradient: LinearGradient(
+                  colors: <Color>[
+                    color2,
+                    color1,
+                    color2,
+                  ],
+                ),
               ),
-            ],
-            gradient: LinearGradient(
-              colors: <Color>[
-                color2,
-                color1,
-                color2,
-              ],
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Icon(
+                    icon,
+                    color: color3,
+                    size: 30,
+                  ),
+                  SizedBox(width: width - 230),
+                  Text(
+                    widget.title,
+                    style: TextStyle(
+                      fontFamily: 'Merriweather',
+                      fontSize: 21,
+                      fontWeight: FontWeight.bold,
+                      color: color3,
+                    )
+                    ),
+                  const SizedBox(width: 10),
+                  Icon(
+                    Icons.arrow_forward_ios_outlined,
+                    color: color3,
+                  ),
+
+                  
+                ],
+                )
             ),
           ),
         ),
--- period_tracker_app/lib/pages/home.dart
+++ period_tracker_app/lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:period_tracker_app/customized_widgets/homeCards.dart';
+import 'package:flutter/rendering.dart';
 
 class Home extends StatefulWidget{
   @override
@@ -9,31 +10,34 @@ class Home extends StatefulWidget{
 class _HomeState extends State{
 
   List<HomeCards> homeCardsList = [
-                                    HomeCards(title: 'Calender'),
+                                    HomeCards(title: 'Calendar'),
                                     HomeCards(title: 'Journal'),
                                     HomeCards(title: 'Profile'),
-                                    HomeCards(title: 'Setting'),
+                                    HomeCards(title: 'Settings'),
                                   ];
-
-  @override
-  Widget build(BuildContext context){
-    return SafeArea(
-      child: Scaffold(
-        body: Column(children: <Widget>[
-            Container(
-              height: 500,
-              child: ListView.builder(
-                itemCount: homeCardsList.length,
-                itemBuilder: (context, index )
-                {
-                  return homeCardsList[index];
-                }
-                ),
-            )
-            
-        
-          ],),
-        )
-      );
-  }
+@override
+Widget build(BuildContext context) {
+  return SafeArea(
+    child: Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Container(
+            height: 300,
+            child: Stack(
+            alignment: Alignment.center,
+            children: List.generate(
+              homeCardsList.length,
+              (index) {
+                return Positioned(
+                  top: 70.0*index, 
+                  child: homeCardsList[index] );}
+                ),  
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
 }
