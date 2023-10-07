8e5b9fd461bd431285b080c5553d7ef889db907e
HalaJah <114840619+HalaJah@users.noreply.github.com>
2023-10-06 22:41:54 -0500

    Structure database

--- period_tracker_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ period_tracker_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,13 +8,17 @@ import Foundation
 import cloud_firestore
 import dynamic_color
 import firebase_core
+import firebase_database
 import macos_ui
 import macos_window_utils
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   MacOSUiPlugin.register(with: registry.registrar(forPlugin: "MacOSUiPlugin"))
   MacOSWindowUtilsPlugin.register(with: registry.registrar(forPlugin: "MacOSWindowUtilsPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
